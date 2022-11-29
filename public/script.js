f1aa25edc8b7db0c4dfab3e01dfa35e4d42b301e
Stanley Surjanto <stan.surjanto@gmail.com>
2022-11-28 21:05:09 -0700

    refactor notes & adjust some timings - nothing major ⭐️

--- public/script.js
+++ public/script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const ctx = canvas.getContext("2d");
   canvas.width = 500;
   canvas.height = 800;
-  // ---- Image resources
+  // ---- Sprite Resources
   const worm = new Image();
   worm.src = "./images/enemy_worm.png";
   const ghost = new Image();
@@ -12,23 +12,23 @@ document.addEventListener("DOMContentLoaded", function () {
   const spider = new Image();
   spider.src = "./images/enemy_spider.png";
 
-  // ---- Main Game Logic
+  // ---- Main Game Logic ----
   class Game {
-    // !NOTE! Any new instance of this class always runs the constructor
+    // !NOTE! Any new instance of this class always runs the constructor FIRST
     constructor(ctx, width, height) {
       this.ctx = ctx;
       this.width = width;
       this.height = height;
       // Holds ALL enemies for instance of a game
       this.enemies = [];
-      // Want to spawn > 1 enemy, at this interval, spawn new
+      // Interval for new Enemy spawns
       this.enemyInterval = 500;
       this.enemyTimer = 0;
-      // Multiple enemies
+      // Multiple enemy types
       this.enemyTypes = ["worm", "ghost", "spider"];
     }
 
-    // These update/draws handles background, enemies, resources etc.
+    // Receives deltaTime from main animation loop
     update(deltaTime) {
       this.enemies = this.enemies.filter((enemy) => !enemy.markedForDeletion);
 
@@ -52,11 +52,10 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Private class method in JS | ONLY call-able inside Game class | call on Instantiation
     #addNewEnemy() {
-      // Randomize index position 
+      // Randomize index position & find random enemies
       const randomIndex = Math.floor(Math.random() * this.enemyTypes.length);
-      // Find random enemy in array
       const randomEnemy = this.enemyTypes[randomIndex];
-      // Generate that enemy type
+      // Generate that type & pass game instance as context
       if (randomEnemy === "worm") this.enemies.push(new Worm(this));
       else if (randomEnemy === "ghost") this.enemies.push(new Ghost(this));
       else if (randomEnemy === "spider") this.enemies.push(new Spider(this));
@@ -69,10 +68,10 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 
-  // ---- Enemy Factory
+  // ---- Enemy Common Logic & Animations ----
   class Enemy {
     constructor(game) {
-      // Able to use all Game class properties
+      // Access all Game class properties
       this.game = game;
       this.markedForDeletion = false;
       // Animation logic all here
@@ -82,7 +81,7 @@ document.addEventListener("DOMContentLoaded", function () {
       this.frameTimer = 0;
     }
 
-    // These update/draws handle SPECIFIC enemy instances
+    // update/draws for SPECIFIC enemy instances
     update(deltaTime) {
       this.x -= this.velX * deltaTime;
 
@@ -116,29 +115,30 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // ---- Worms
   class Worm extends Enemy {
-    // !NOTE! super() in constructor runs its parent Enemy class' constructor FIRST
+    // !NOTE! super() runs its parent class' constructor or methods
     constructor(game) {
       super(game);
+      this.image = worm;
       // Sprite FRAME dimensions
       this.spriteWidth = 229;
       this.spriteHeight = 171;
-      // On CANVAS dimensions
+      // Scale on CANVAS dimensions
       this.width = this.spriteWidth * 0.5;
       this.height = this.spriteWidth * 0.5;
       // Spawn to right of canvas
       this.x = this.game.width;
       // Worms spawn only on "ground"
       this.y = this.game.height - this.height;
-      this.image = worm;
       this.velX = Math.random() * 0.1 + 0.1;
     }
-    // !NOTE! When Game tries to find update() or draw(), it keeps looking up parent class until it finds it
+    // !NOTE! When Game tries to find update() or draw(), keeps looking up parent class until it finds it
   }
 
   // ---- Spiders
   class Spider extends Enemy {
     constructor(game) {
       super(game);
+      this.image = spider;
       this.spriteWidth = 310;
       this.spriteHeight = 175;
       this.width = this.spriteWidth * 0.35;
@@ -146,7 +146,6 @@ document.addEventListener("DOMContentLoaded", function () {
       // Spawn anywhere side to side on canvas but above canvas
       this.x = Math.random() * this.game.width;
       this.y = 0 - this.height;
-      this.image = spider;
       this.velX = 0;
       this.velY = Math.random() * 0.1 + 0.1;
       // Randomize how far each spider drops down
@@ -154,32 +153,34 @@ document.addEventListener("DOMContentLoaded", function () {
     }
     update(deltaTime) {
       super.update(deltaTime);
+
       // !NOTE! Spiders START at (0 - this.height), ONLY DELETE at (0 - this.height * 2)
       if (this.y < 0 - this.height * 2) {
         this.markedForDeletion = true;
       }
-      // deltaTime normalize for PCs so makesure the velocity is small
+
+      // deltaTime normalize for PCs, velocity is small
       this.y += this.velY * deltaTime;
-      // Make them bounce up and down by reversing their direction at threshold
+      
+      // Bounce up and down by reversing their direction at threshold
       if (this.y > this.maxDropDown) this.velY *= -1;
 
     }
     draw(ctx) {
-      ctx.beginPath(); // New shape draw
+      ctx.beginPath(); // New shape draw | particularly, a line
       ctx.moveTo(this.x + (this.width * 0.5), 0); // Where the line starts from
       // Offset by half the spider dimensions so the line is centered
       ctx.lineTo(this.x + (this.width * 0.5), this.y + (this.height * 0.5));
       ctx.stroke();
       super.draw(ctx);
-
     }
   }
 
   // ---- Ghosts
   class Ghost extends Enemy {
-    // !NOTE! super() in constructor runs its parent Enemy class' constructor FIRST
     constructor(game) {
       super(game);
+      this.image = ghost;
       this.spriteWidth = 261;
       this.spriteHeight = 209;
       this.width = this.spriteWidth * 0.5;
@@ -187,24 +188,22 @@ document.addEventListener("DOMContentLoaded", function () {
       this.x = this.game.width;
       // Ghosts should only spawn in top half of canvas
       this.y = Math.random() * (this.game.height * 0.5);
-      this.image = ghost;
       this.velX = Math.random() * 0.2 + 0.1;
       // Flying Pattern
       this.angle = 0;
-      this.curve = Math.random() * 2;
+      this.curve = Math.random() * 3;
     }
     update(deltaTime) {
       super.update(deltaTime);
       this.y += Math.sin(this.angle) + this.curve;
-      this.angle += 0.04;
+      this.angle += 0.3;
     }
 
     draw(ctx) {
-      // SAVE && RESTORE -> snapshots canvas BEFORE running below code and restores it
+      // "save" && "restore" snapshots canvas BEFORE running below code and restores it
       ctx.save();
       // Meaning -> ONLY ghosts will be affected by this global setting on spawn
-      ctx.globalAlpha = 0.8; // BY ITSELF -> affects all canvas properties
-      // Want the DEFAULT draw() method from Enemy class && something unique to Ghosts!
+      ctx.globalAlpha = 0.8; // BY ITSELF -> will affect all canvas properties
       super.draw(ctx) // AS IF -> Enemy.draw(ctx)
       ctx.restore();
     }
@@ -212,7 +211,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // ---- Game Instance
   const game = new Game(ctx, canvas.width, canvas.height);
-  // Init val avoids undefined - reassigned each loop | For frame equalize /b/ bad & good PCs
+  // Init val avoids undefined - reassigned each loop | Normalize frames /b/ bad & good PCs
   let lastTime = 1;
 
   // ---- Main Animation Loop -----
@@ -220,14 +219,12 @@ document.addEventListener("DOMContentLoaded", function () {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     // -- Frame Timings
-    // timeStamp built-in requestAnimationFrame();
+    // timeStamp built-in with requestAnimationFrame();
     const deltaTime = timeStamp - lastTime;
     lastTime = timeStamp;
     // -- Invoke Game Methods
-    // !NOTE! deltaTime caps enemy spawn at ~1000ms for all PCs
     game.update(deltaTime);
-    game.draw();
-
+    
     requestAnimationFrame(animate);
   }
 
